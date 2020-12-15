f486b08fa4e1e66f06705452186c364e4c90ef15
besrour walid <walidbesrour@gmail.com>
2020-12-15 09:44:31 +0100

    tp1 tp2

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -1,9 +1,7 @@
 package com.mycompany.dvdstore;
 
-import com.mycompany.dvdstore.entity.Movie;
-import com.mycompany.dvdstore.service.MovieService;
 
-import java.util.Scanner;
+import com.mycompany.dvdstore.controller.MovieController;
 
 /**
  * Hello world!
@@ -13,15 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "What is the movie title ?" );
-        Scanner scanner=new Scanner(System.in);
-        String title=scanner.nextLine();
-        System.out.println( "What is the movie genre?" );
-        String genre=scanner.nextLine();
-        Movie movie=new Movie();
-        movie.setTitle(title);
-        movie.setGenre(genre);
-        MovieService movieService=new MovieService();
-        movieService.registerMovie(movie);
+        MovieController movieController=new MovieController();
+        movieController.addUsingConsole();
     }
 }
--- src/main/java/com/mycompany/dvdstore/service/MovieService.java
+++ src/main/java/com/mycompany/dvdstore/service/MovieService.java
@@ -1,6 +1,7 @@
 package com.mycompany.dvdstore.service;
 
 import com.mycompany.dvdstore.entity.Movie;
+import com.mycompany.dvdstore.repository.GoLiveMovieRepository;
 import com.mycompany.dvdstore.repository.MovieRepository;
 
 import java.util.ArrayList;
@@ -8,10 +9,11 @@ import java.util.List;
 
 public class MovieService {
 
-    private MovieRepository movieRepository = new MovieRepository();
+//    private MovieRepository movieRepository = new MovieRepository();
+    private GoLiveMovieRepository goLiveMovieRepository = new GoLiveMovieRepository();
 
     public void registerMovie(Movie movie){
-    movieRepository.add(movie);
+        goLiveMovieRepository.add(movie);
     }
 
 }
